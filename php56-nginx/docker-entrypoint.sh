b6fb2148ee91fa9f06a11876b1a39ec9eaf398cd
HuyNQ <huy@huynq.net>
2016-06-15 23:53:22 +0700

    Update scripts & entry-points

--- php56-nginx/docker-entrypoint.sh
+++ php56-nginx/docker-entrypoint.sh
@@ -6,6 +6,7 @@ if [ -f "/var/run/php-fpm.pid" ]; then
   if [ ! -z ${pid} ]; then
     echo "PHP-FPM is running. Killing pid ${pid}"
     kill -QUIT ${pid}
+    rm /var/run/php-fpm.pid
   fi
 fi
 
@@ -14,6 +15,7 @@ if [ -f "/var/run/nginx.pid" ]; then
   if [ ! -z ${pid} ]; then
     echo "Nginx is running. Killing pid ${pid}"
     kill -QUIT ${pid}
+    rm /var/run/nginx.pid
   fi
 fi
 
