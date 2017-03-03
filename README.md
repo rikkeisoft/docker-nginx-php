0cbc962d30a604b21dc23d022b347ba06482cadc
HuyNQ <huy@huynq.net>
2017-03-03 19:48:36 +0900

    Upgrade php7-nginx to PHP-FPM 7.1.2 & Nginx 1.11.9

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # docker-nginx-php
 Dockerized Nginx + PHP
 
-- [Alpine 3.4 + PHP 7.0 + Nginx 1.11](/php7-nginx)
-- [Alpine 3.4 + PHP 5.6 + Nginx 1.11](/php56-nginx)
+- [Alpine 3.4 + PHP-FPM 7.1.2 + Nginx 1.11.9](/php7-nginx)
+- [Alpine 3.4 + PHP-FPM 5.6.22 + Nginx 1.11.1](/php56-nginx)
--- php7-nginx/docker-entrypoint.sh
+++ php7-nginx/docker-entrypoint.sh
@@ -2,20 +2,24 @@
 set -e
 
 if [ -f "/var/run/php-fpm.pid" ]; then
-  local pid=`cat /var/run/php-fpm.pid`
-  if [ ! -z ${pid} ]; then
-    echo "PHP-FPM is running. Killing pid ${pid}"
-    kill -QUIT ${pid}
-    echo "" > /var/run/php-fpm.pid
+  fpm_pid=`cat /var/run/php-fpm.pid`
+  if [ ! -z ${fpm_pid} ]; then
+    if (ps -p $fpm_pid > /dev/null); then
+      echo "PHP-FPM is running. Killing pid ${fpm_pid}"
+      kill -QUIT ${fpm_pid}
+      echo "" > /var/run/php-fpm.pid
+    fi
   fi
 fi
 
 if [ -f "/var/run/nginx.pid" ]; then
-  local pid=`cat /var/run/nginx.pid`
-  if [ ! -z ${pid} ]; then
-    echo "Nginx is running. Killing pid ${pid}"
-    kill -QUIT ${pid}
-    echo "" > /var/run/nginx.pid
+  nginx_pid=`cat /var/run/nginx.pid`
+  if [ ! -z ${nginx_pid} ]; then
+    if (ps -p $nginx_pid > /dev/null); then
+      echo "Nginx is running. Killing pid ${nginx_pid}"
+      kill -QUIT ${nginx_pid}
+      echo "" > /var/run/nginx.pid
+    fi
   fi
 fi
 
